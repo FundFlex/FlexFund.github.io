ad1a3c5213bedb64ca5aa952e34267ee1c9aeaa6
Dylan Lee <leedy15@gmail.com>
2019-06-04 09:39:19 -0700

    first push
    first push

---
+++ FundFlex/FundFlex/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FundFlex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
